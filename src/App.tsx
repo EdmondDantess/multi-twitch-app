bffdc0a6824018f801ce5c3ef2ae9214526e915b
Maksim Laurouski <maximlavrovsky@mail.ru>
2023-01-04 17:31:02 +0300

    feat: added library react-twitch-embed-video

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactTwitchEmbedVideo from "react-twitch-embed-video"
 
 function App() {
     return <div className="App">
-
-        <iframe src="https://player.twitch.tv/?channel=dreadztv&parent=www.multi-twitch-app.vercel.app/"
-                frameBorder="0"
-                allowFullScreen scrolling="no" height="378" width="620"></iframe>
+        <ReactTwitchEmbedVideo channel="dreadztv" layout={'video'} height={'300px'} />
+        <ReactTwitchEmbedVideo channel="bratiki94" layout={'video'} height={'300px'} />
+        <ReactTwitchEmbedVideo channel="restoratorgame" layout={'video'} height={'300px'} />
+        <ReactTwitchEmbedVideo channel="tommykaylive" layout={'video'} height={'300px'} />
     </div>
 
 }
