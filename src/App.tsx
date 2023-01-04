14ab6ddd429ae4b12c92f8c15662893844b51f1d
Maksim Laurouski <maximlavrovsky@mail.ru>
2023-01-04 16:51:40 +0300

    1 test commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ import './App.css';
 function App() {
     return <div className="App">
 
-               <iframe src="https://player.twitch.tv/?channel=dreadztv&parent=https://multi-twitch-app.vercel.app/" frameBorder="0"
-                       allowFullScreen scrolling="no" height="378" width="620"></iframe>
+        <iframe src="https://player.twitch.tv/?channel=dreadztv&parent=www.multi-twitch-app.vercel.app/"
+                frameBorder="0"
+                allowFullScreen scrolling="no" height="378" width="620"></iframe>
     </div>
 
 }
