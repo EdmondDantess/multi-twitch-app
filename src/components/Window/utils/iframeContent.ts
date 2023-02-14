75a01a81cd3706ec1779003e1dc0906480733e8e
Maksim Laurouski <maximlavrovsky@mail.ru>
2023-02-14 14:37:49 +0300

    fix: mode in iframes

--- src/components/Window/utils/iframeContent.ts
+++ src/components/Window/utils/iframeContent.ts
@@ -1,8 +1,12 @@
-import {mode} from '../../../common/utils/modeLocalToVercel';
+import {mode, vercel} from '../../../common/utils/modeLocalToVercel';
 
 export const contentVideo_URL = (channel: string) => {
-    return `https://embed.twitch.tv?allowfullscreen=true&autoplay=true&channel=${channel}&controls=true&height=100%25&layout=video&muted=false&parent=localhost&referrer=${mode}&theme=dark&time=0h0m0s&width=100%25`
+    const vercelVid = `https://embed.twitch.tv?allowfullscreen=true&autoplay=true&channel=${channel}&controls=true&height=100%25&layout=video&muted=false&parent=multi-twitch-app.vercel.app&referrer=https%3A%2F%2Fmulti-twitch-app.vercel.app2F&theme=dark&time=0h0m0s&width=100%25`
+    const localHVid = `https://embed.twitch.tv?allowfullscreen=true&autoplay=true&channel=${channel}&controls=true&height=100%25&layout=video&muted=false&parent=localhost&referrer=http%3A%2F%2Flocalhost%3A3000%2F&theme=dark&time=0h0m0s&width=100%25`
+    return mode === vercel ? vercelVid : localHVid
 }
 export const contentChat_URL = (channel: string) => {
-    return   `https://www.twitch.tv/embed/${channel}/chat?parent=${mode}&referrer=${mode}&darkpopout`
+    const vercelChat = `https://www.twitch.tv/embed/${channel}/chat?parent=multi-twitch-app.vercel.app&referrer=https%3A%2F%2Fmulti-twitch-app.vercel.app&darkpopout`
+    const localHChat = `https://www.twitch.tv/embed/${channel}/chat?parent=localhost&referrer=http%3A%2F%2Flocalhost%3A3000%&`
+    return mode === vercel ? vercelChat : localHChat
 }
