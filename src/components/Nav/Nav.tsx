dd3385d93afd781958f2968b6a1ed53d4e74b22d
Maksim Laurouski <maximlavrovsky@mail.ru>
2024-02-21 19:27:06 +0300

    feat: settings for eslint and prettier

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,9 @@
 import {
-    addNewWindow,
-    deleteWindow,
-    WindowType,
+  addNewWindow,
+  deleteWindow,
+  WindowType,
 } from '../Window/window-reducer'
-import React, { useEffect } from 'react'
+import React, { memo, useEffect } from 'react'
 import './nav.css'
 import close from '../../assets/icons/close.png'
 import red_dot from '../../assets/icons/red_dot.png'
@@ -15,185 +15,165 @@ import { tokenFromURL } from '../../common/utils/getTokenFromURL'
 import { setError } from '../../app/userFeedback-reducer'
 import { DataRecommends, LiveFollowsDataData } from '../../api/twitchAPI'
 
-export const Nav = React.memo(() => {
-    const dispatch = useAppDispatch()
-    const windows = useAppSelector((state) => state.window.windows)
-    const userData = useAppSelector((state) => state.nav.userData)
-    const liveFollows = useAppSelector((state) => state.nav.liveFollows)
-    const additinalData = useAppSelector((state) => state.nav.additionalData)
-    const myFollows = useAppSelector((state) => state.nav.myFollows)
-    const recommendsData = useAppSelector(
-        (state) => state.nav.recommendedStreams
-    )
-    const isLogged = tokenMode() !== tokenFromURL
+export const Nav = memo(() => {
+  const dispatch = useAppDispatch()
+  const windows = useAppSelector((state) => state.window.windows)
+  const userData = useAppSelector((state) => state.nav.userData)
+  const liveFollows = useAppSelector((state) => state.nav.liveFollows)
+  const additinalData = useAppSelector((state) => state.nav.additionalData)
+  const myFollows = useAppSelector((state) => state.nav.myFollows)
+  const recommendsData = useAppSelector((state) => state.nav.recommendedStreams)
+  const isLogged = tokenMode() !== tokenFromURL
 
-    useEffect(() => {
-        !userData.id && isLogged && dispatch(getUserData())
-        if (userData.id) {
-            isLogged && dispatch(getMyFollows(userData.id))
-            isLogged && dispatch(getRecommendedStreams())
-        }
-    }, [userData.id, dispatch, isLogged])
-
-    useEffect(() => {
-        let idIntervalLiveFollows = setInterval(() => {
-            console.log('mylives')
-            dispatch(getMyFollows(userData.id))
-        }, 300000)
-        let idIntervalRecommends = setInterval(() => {
-            console.log('recommends')
-            dispatch(getRecommendedStreams())
-        }, 1500000)
-
-        return () => {
-            console.log('cleared')
-            clearInterval(idIntervalLiveFollows)
-            clearInterval(idIntervalRecommends)
-        }
-    }, [userData.id, isLogged])
-
-    const generateListChannelsOnBoard = () => {
-        return windows.map((w: WindowType) => {
-            return (
-                <div className={'nav__channel'} key={w.channel}>
-                    {w.channel}
-                    <div
-                        className={'nav__channels-delete'}
-                        onClick={() => dispatch(deleteWindow(w.channel))}
-                    >
-                        <img src={close} alt={'delete icon'} />
-                    </div>
-                </div>
-            )
-        })
+  useEffect(() => {
+    !userData.id && isLogged && dispatch(getUserData())
+    if (userData.id) {
+      isLogged && dispatch(getMyFollows(userData.id))
+      isLogged && dispatch(getRecommendedStreams())
     }
+  }, [userData.id, dispatch, isLogged])
 
-    const generateMyFollows = () => {
-        let follows: LiveFollowsDataData[] = [...liveFollows]
-        return follows.map((f) => {
-            const addOnBoard = () => {
-                windows.filter((w) => w.channel === f.user_name).length === 0
-                    ? dispatch(addNewWindow(f.user_name))
-                    : dispatch(setError(`${f.user_name} is exist on board`))
-            }
-            let currUser = myFollows?.find((el) => el.id === f.user_id)
+  useEffect(() => {
+    const idIntervalLiveFollows = setInterval(() => {
+      console.log('mylives')
+      dispatch(getMyFollows(userData.id))
+    }, 300000)
+    const idIntervalRecommends = setInterval(() => {
+      console.log('recommends')
+      dispatch(getRecommendedStreams())
+    }, 1500000)
 
-            return (
-                <div
-                    className={'nav__my-follow'}
-                    key={f.id}
-                    onClick={addOnBoard}
-                    title={f.title}
-                >
-                    <div
-                        className={
-                            'relative  flex flex-row overflow-clip whitespace-nowrap'
-                        }
-                    >
-                        <img
-                            src={currUser?.profile_image_url}
-                            alt="avatar"
-                            width={'48px'}
-                            className={
-                                'rounded-r-2xl border-2 border-purple-500 bg-black'
-                            }
-                        />
-                        <div className={'w-full'} title={f.title}>
-                            <div className={'w-full text-left'}>
-                                {f.user_name}
-                            </div>
-                            <div className={'w-full text-left text-sm'}>
-                                {f.game_name}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )
-        })
+    return () => {
+      console.log('cleared')
+      clearInterval(idIntervalLiveFollows)
+      clearInterval(idIntervalRecommends)
     }
+  }, [userData.id, isLogged])
 
-    const generateMyRecommends = () => {
-        return recommendsData.map((r: DataRecommends) => {
-            if (myFollows.find((el) => el.login === r.user_login)) return null
-            const addOnBoard = () => {
-                windows.filter((w) => w.channel === r.user_login).length === 0
-                    ? dispatch(addNewWindow(r.user_login))
-                    : dispatch(setError(`${r.user_login} is exist on board`))
-            }
-            let user = additinalData?.find((el) => el.id === r.user_id)
-            return (
-                <div
-                    className={'nav__my-follow'}
-                    key={r.id}
-                    onClick={addOnBoard}
-                >
-                    <img
-                        src={user?.profile_image_url}
-                        alt="avatar"
-                        width={'48px'}
-                        className={
-                            'rounded-r-2xl border-2 border-purple-500 bg-black'
-                        }
-                    />
-                    <div
-                        className={
-                            'flex w-full flex-col overflow-clip whitespace-nowrap text-left'
-                        }
-                    >
-                        <div className={'font-bold'}>{r.user_name}</div>
-                        <div className={'text-right text-sm'}>
-                            {r.game_name}
-                        </div>
-                    </div>
-                    <img src={red_dot} alt="live" className={'h-4'} />
-                </div>
-            )
-        })
-    }
-
-    return (
-        <div className={'nav'}>
-            <div className={'nav__userinfo'} title={'open settings'}>
-                <div className={'nav__userinfo__nickname'}>
-                    <a
-                        href="https://www.twitch.tv/settings/profile"
-                        target={'_blank'}
-                        rel={'noreferrer'}
-                    >
-                        <img src={userData?.profile_image_url} alt="avatar" />
-                        <div className={'nav__userinfo__nickname-wrapper'}>
-                            <span>You logged as:</span>
-                            <span>{userData?.login}</span>
-                            <span>{userData?.email}</span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <Search />
-            {generateMyFollows().length > 0 && (
-                <>
-                    <span className={'font-bold'}>
-                        My subscribes in
-                        <span className={'rounded-[4px] bg-red-600 p-[1px]'}>
-                            Live
-                        </span>
-                        ({myFollows.length}):
-                    </span>
-                    <div className={'nav__my-follows'}>
-                        {generateMyFollows()}
-                    </div>
-                </>
-            )}
-            <span className={'font-bold'}>Other streams:</span>
-            <div className={'nav__my-follows'}>{generateMyRecommends()}</div>
-            {generateListChannelsOnBoard().length > 0 && (
-                <>
-                    <span>Channels on board:</span>
-                    <div className={'nav__channels-onboard'}>
-                        {generateListChannelsOnBoard()}
-                    </div>
-                </>
-            )}
+  const generateListChannelsOnBoard = () => {
+    return windows.map((w: WindowType) => {
+      return (
+        <div className={'nav__channel'} key={w.channel}>
+          {w.channel}
+          <div
+            className={'nav__channels-delete'}
+            onClick={() => dispatch(deleteWindow(w.channel))}
+          >
+            <img src={close} alt={'delete icon'} />
+          </div>
         </div>
-    )
+      )
+    })
+  }
+
+  const generateMyFollows = () => {
+    const follows: LiveFollowsDataData[] = [...liveFollows]
+    return follows.map((f) => {
+      const addOnBoard = () => {
+        windows.filter((w) => w.channel === f.user_name).length === 0
+          ? dispatch(addNewWindow(f.user_name))
+          : dispatch(setError(`${f.user_name} is exist on board`))
+      }
+      const currUser = myFollows?.find((el) => el.id === f.user_id)
+
+      return (
+        <div
+          className={'nav__my-follow'}
+          key={f.id}
+          onClick={addOnBoard}
+          title={f.title}
+        >
+          <div
+            className={
+              'relative  flex flex-row overflow-clip whitespace-nowrap'
+            }
+          >
+            <img
+              src={currUser?.profile_image_url}
+              alt='avatar'
+              width={'48px'}
+              className={'rounded-r-2xl border-2 border-purple-500 bg-black'}
+            />
+            <div className={'w-full'} title={f.title}>
+              <div className={'w-full text-left'}>{f.user_name}</div>
+              <div className={'w-full text-left text-sm'}>{f.game_name}</div>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  const generateMyRecommends = () => {
+    return recommendsData.map((r: DataRecommends) => {
+      if (myFollows.find((el) => el.login === r.user_login)) return null
+      const addOnBoard = () => {
+        windows.filter((w) => w.channel === r.user_login).length === 0
+          ? dispatch(addNewWindow(r.user_login))
+          : dispatch(setError(`${r.user_login} is exist on board`))
+      }
+      const user = additinalData?.find((el) => el.id === r.user_id)
+      return (
+        <div className={'nav__my-follow'} key={r.id} onClick={addOnBoard}>
+          <img
+            src={user?.profile_image_url}
+            alt='avatar'
+            width={'48px'}
+            className={'rounded-r-2xl border-2 border-purple-500 bg-black'}
+          />
+          <div
+            className={
+              'flex w-full flex-col overflow-clip whitespace-nowrap text-left'
+            }
+          >
+            <div className={'font-bold'}>{r.user_name}</div>
+            <div className={'text-right text-sm'}>{r.game_name}</div>
+          </div>
+          <img src={red_dot} alt='live' className={'h-4'} />
+        </div>
+      )
+    })
+  }
+
+  return (
+    <div className={'nav'}>
+      <div className={'nav__userinfo'} title={'open settings'}>
+        <div className={'nav__userinfo__nickname'}>
+          <a
+            href='https://www.twitch.tv/settings/profile'
+            target={'_blank'}
+            rel={'noreferrer'}
+          >
+            <img src={userData?.profile_image_url} alt='avatar' />
+            <div className={'nav__userinfo__nickname-wrapper'}>
+              <span>You logged as:</span>
+              <span>{userData?.login}</span>
+              <span>{userData?.email}</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <Search />
+      {generateMyFollows().length > 0 && (
+        <>
+          <span className={'font-bold'}>
+            My subscribes in
+            <span className={'rounded-[4px] bg-red-600 p-[1px]'}>Live</span>(
+            {myFollows.length}):
+          </span>
+          <div className={'nav__my-follows'}>{generateMyFollows()}</div>
+        </>
+      )}
+      <span className={'font-bold'}>Other streams:</span>
+      <div className={'nav__my-follows'}>{generateMyRecommends()}</div>
+      {generateListChannelsOnBoard().length > 0 && (
+        <>
+          <span>Channels on board:</span>
+          <div className={'nav__channels-onboard'}>
+            {generateListChannelsOnBoard()}
+          </div>
+        </>
+      )}
+    </div>
+  )
 })
--- src/components/Nav/Search/Search.tsx
+++ src/components/Nav/Search/Search.tsx
@@ -7,105 +7,98 @@ import { useAppDispatch, useAppSelector } from '../../../app/hooks'
 import close from '../../../assets/icons/close.png'
 
 export const Search = React.memo(() => {
-    const dispatch = useAppDispatch()
-    const windows = useAppSelector((state) => state.window.windows)
-    const searchingChannels = useAppSelector(
-        (state) => state.search.searchingChannels
-    )
-    const [searchValue, setSearchValue] = useState<string>('')
-    const [error, setError] = useState<string | null>(null)
-    const search = useDebounce<string>(searchValue, 500)
+  const dispatch = useAppDispatch()
+  const windows = useAppSelector((state) => state.window.windows)
+  const searchingChannels = useAppSelector(
+    (state) => state.search.searchingChannels
+  )
+  const [searchValue, setSearchValue] = useState<string>('')
+  const [error, setError] = useState<string | null>(null)
+  const search = useDebounce<string>(searchValue, 500)
 
-    useEffect(() => {
-        if (searchValue.trim() && search.trim() !== '') {
-            dispatch(getSearchChannels(searchValue.trim()))
-        }
-        if (searchValue.trim() === '') {
-            dispatch(setSearchChannels([]))
-        }
-    }, [dispatch, search, searchValue])
-
-    function addWindowOnBoard(channel: string) {
-        windows.find((c) => c.channel === channel)
-            ? setError('Channel is exist')
-            : dispatch(addNewWindow(channel))
-        clearSearchInput()
+  useEffect(() => {
+    if (searchValue.trim() && search.trim() !== '') {
+      dispatch(getSearchChannels(searchValue.trim()))
     }
-
-    function clearSearchInput() {
-        dispatch(setSearchChannels([]))
-        setSearchValue('')
+    if (searchValue.trim() === '') {
+      dispatch(setSearchChannels([]))
     }
+  }, [dispatch, search, searchValue])
 
-    return (
-        <div className={'search'}>
-            {error && <div className={'search__error'}>{error}</div>}
-            <div className={'search__input'}>
-                <input
-                    placeholder={'Enter channel name'}
-                    type="text"
-                    value={searchValue}
-                    onChange={(e) =>
-                        e.currentTarget.value !== ';'
-                            ? setSearchValue(e.currentTarget.value)
-                            : null
-                    }
+  function addWindowOnBoard(channel: string) {
+    windows.find((c) => c.channel === channel)
+      ? setError('Channel is exist')
+      : dispatch(addNewWindow(channel))
+    clearSearchInput()
+  }
+
+  function clearSearchInput() {
+    dispatch(setSearchChannels([]))
+    setSearchValue('')
+  }
+
+  return (
+    <div className={'search'}>
+      {error && <div className={'search__error'}>{error}</div>}
+      <div className={'search__input'}>
+        <input
+          placeholder={'Enter channel name'}
+          type='text'
+          value={searchValue}
+          onChange={(e) =>
+            e.currentTarget.value !== ';'
+              ? setSearchValue(e.currentTarget.value)
+              : null
+          }
+        />
+        {searchValue !== '' && (
+          <div onClick={clearSearchInput} className={'search__btn_clear'}>
+            <img src={close} alt='clear' />
+          </div>
+        )}
+      </div>
+      {searchingChannels.length > 0 && (
+        <div className={'search__channels-result'}>
+          {searchingChannels?.map((c, index) => {
+            return (
+              <div
+                key={index}
+                className={'search__channel-result'}
+                onClick={() => addWindowOnBoard(c.broadcaster_login)}
+              >
+                <img
+                  src={c.thumbnail_url}
+                  alt='streamer avatar'
+                  className={'search__channel-result__avatar'}
                 />
-                {searchValue !== '' && (
-                    <div
-                        onClick={clearSearchInput}
-                        className={'search__btn_clear'}
-                    >
-                        <img src={close} alt="clear" />
-                    </div>
-                )}
-            </div>
-            {searchingChannels.length > 0 && (
-                <div className={'search__channels-result'}>
-                    {searchingChannels?.map((c, index) => {
-                        return (
-                            <div
-                                key={index}
-                                className={'search__channel-result'}
-                                onClick={() =>
-                                    addWindowOnBoard(c.broadcaster_login)
-                                }
-                            >
-                                <img
-                                    src={c.thumbnail_url}
-                                    alt="streamer avatar"
-                                    className={'search__channel-result__avatar'}
-                                />
-                                <div>
-                                    <div>{c.display_name}</div>
-                                    <div className={'text-xs'}>
-                                        {c.title.length > 20 ? (
-                                            <span title={c.title}>
-                                                {c.title
-                                                    .slice(0, 20)
-                                                    .concat('...')}
-                                            </span>
-                                        ) : (
-                                            <span>{c.game_name}</span>
-                                        )}
-                                    </div>
-                                </div>
-                                <div>
-                                    {c.is_live && (
-                                        <span
-                                            className={
-                                                'absolute right-0  rounded-ee bg-red-600 font-bold text-white'
-                                            }
-                                        >
-                                            Live
-                                        </span>
-                                    )}
-                                </div>
-                            </div>
-                        )
-                    })}
+                <div>
+                  <div>{c.display_name}</div>
+                  <div className={'text-xs'}>
+                    {c.title.length > 20 ? (
+                      <span title={c.title}>
+                        {c.title.slice(0, 20).concat('...')}
+                      </span>
+                    ) : (
+                      <span>{c.game_name}</span>
+                    )}
+                  </div>
                 </div>
-            )}
+                <div>
+                  {c.is_live && (
+                    <span
+                      className={
+                        'absolute right-0  rounded-ee bg-red-600 font-bold text-white'
+                      }
+                    >
+                      Live
+                    </span>
+                  )}
+                </div>
+              </div>
+            )
+          })}
         </div>
-    )
+      )}
+    </div>
+  )
 })
